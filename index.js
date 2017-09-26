02ae709eeba1f5951ca972bb94503372eee645f7
kodo <524580860@qq.com>
2017-09-26 14:33:03 +0800

    add: setup option to extend your app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var webpack = require('webpack');
+var FriendLyErrorsPlugin = require('friendly-errors-webpack-plugin');
 
 var app = express();
 
 function WebpackBrowserLog(webpackConfig, options) {
+  webpackConfig.plugins.push(new FriendLyErrorsPlugin(options.errorsPluginOptions || {}));
   this.init(webpackConfig, options || {});
 }
 
@@ -12,12 +14,13 @@ WebpackBrowserLog.prototype = {
   init: function init(webpackConfig, options) {
     var compiler = webpack(webpackConfig);
     var devMiddleConfig = options.devMiddleware || { // https://github.com/webpack/webpack-dev-middleware#usage
-        publicPath: webpackConfig.output.publicPath,
-        quiet: true,
-      };
-		var hotMiddleConfig = options.hotMiddleware || { // https://github.com/glenjamin/webpack-hot-middleware#config
-        log: () => {},
-      };
+      publicPath: webpackConfig.output.publicPath,
+      quiet: true,
+    };
+    var hotMiddleConfig = options.hotMiddleware || { // https://github.com/glenjamin/webpack-hot-middleware#config
+      log: () => {},
+      heartbeat: 2000,
+    };
     var devMiddleware = this.devMiddleware(compiler, devMiddleConfig);
 
     var hotMiddleware = this.hotMiddleware(compiler, hotMiddleConfig);
@@ -26,20 +29,25 @@ WebpackBrowserLog.prototype = {
 
     app.use(hotMiddleware);
 
-    devMiddleware.waitUntilValid(options.waitUntilValid || function waitUntiValid() {
-      });
+    options.setup(app, express);
 
-    app.listen(options.port || 3000, function error(err) {
-      if (err) {
-        console.log(err);
-      }
+    devMiddleware.waitUntilValid(options.waitUntilValid || function waitUntiValid() {
     });
+
+    this.listen(options.port);
   },
   devMiddleware: function devMiddleware(compiler, devOptions) {
     return require('webpack-dev-middleware')(compiler, devOptions);
   },
   hotMiddleware: function hotMiddleware(compiler, hotOptions) {
     return require('webpack-hot-middleware')(compiler, hotOptions);
+  },
+  listen: function listen(port) {
+    app.listen(port || 3000, function error(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   },
 };
 
